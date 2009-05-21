276bc1e94adc75fadb0d8fbbd6c7245919134b00
Michael Meier <flowmaster@gloenggi.ch>
2009-05-21 21:16:13 +0200

    params.lua: new more generic configuration mechanism

--- params.lua
+++ params.lua
@@ -2,12 +2,23 @@
 -- params.lua
 -- (C) 2009, Michael Meier
 
-port = tonumber(argv[1]) or 8001
+if argv[1] then
+   local file = io.open(argv[1])
+   if file then
+      file = nil
+      dofile(argv[1])
+   end
+else
+   port = tonumber(argv[1]) or 8001
+end
+
+port = port or 8001
 --id = string.sub(string.rep(tostring(port), 20), 1, 20)
-id = ec.sha1(tostring(port))
+id = id or ec.sha1(tostring(port))
 
-myaddr = "192.168.1.5"
+--myaddr = "192.168.1.5"
+myaddr = myaddr or "127.0.0.1"
 
-bootstrap = {{addr=myaddr, port=8001},
-	     {addr=myaddr, port=8002}}
+bootstrap = bootstrap or {{addr=myaddr, port=8001},
+			  {addr=myaddr, port=8002}}
 
