d5816a364aafe0ce369b2a38122534b9dd05b5bc
Michael Meier <flowmaster@gloenggi.ch>
2009-06-24 21:13:12 +0200

    kademlua.lua: removed annoying print statements

--- kademlua.lua
+++ kademlua.lua
@@ -20,8 +20,8 @@ math.randomseed(ec.time())
 --print("s1:", s1.readyq)
 
 print("estate:", estate)
-print("hash of 'kademlua':", ec.tohex(ec.sha1("kademlua")))
-print("with fromhex:", ec.tohex(ec.fromhex(ec.tohex(ec.sha1("kademlua")))))
+--print("hash of 'kademlua':", ec.tohex(ec.sha1("kademlua")))
+--print("with fromhex:", ec.tohex(ec.fromhex(ec.tohex(ec.sha1("kademlua")))))
 
 -- this xors
 -- 1111 0101 1100 1010 0.....
@@ -29,11 +29,11 @@ print("with fromhex:", ec.tohex(ec.fromhex(ec.tohex(ec.sha1("kademlua")))))
 -- --------------------------
 -- 0000 0110 1100 1001 0.....
 --    0    6    c    0 0.....
-xorres = ec.xor(ec.fromhex("f5ca0000"), ec.fromhex("f3030000"))
-print("xor:", ec.tohex(xorres))
+--xorres = ec.xor(ec.fromhex("f5ca0000"), ec.fromhex("f3030000"))
+--print("xor:", ec.tohex(xorres))
 -- seems to be working :P
 
-print("this would be put in bucket " .. tostring(ec.getbucketno(xorres)))
+--print("this would be put in bucket " .. tostring(ec.getbucketno(xorres)))
 -- seems to be correct too
 
 
